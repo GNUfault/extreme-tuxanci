ad3359a9ab01b0c2f5a0111aecab8f82356da645
xHire <xHire@0ee4d065-81f0-4d1e-b06c-ff20ad07cc3e>
2008-07-25 10:57:55 +0000

    - upgraded french language file - added first version of packaging scripts for Linux platform and sources
    git-svn-id: http://opensvn.csie.org/tuxanci_ng@165 0ee4d065-81f0-4d1e-b06c-ff20ad07cc3e

---
+++ packaging/source/source.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "*** Source Packager ***"
+
+if [ ! $1 ]; then
+	echo "usage: $0 version"
+	exit 1
+fi
+
+VERSION=$1
+INSTALL_DIR="pkgs/tuxanci-${VERSION}-src"
+SVN='https://opensvn.csie.org/tuxanci_ng/'
+error="Error! See pkgs/src.log for more iformation"
+
+2&>1
+
+rm -rfv $INSTALL_DIR > pkgs/src.log || `echo $error; exit 1`
+
+mkdir -pv pkgs >> pkgs/src.log || `echo $error; exit 1`
+
+echo "* Fetching files from SVN"
+svn export $SVN pkgs/tuxanci-0.20.0-src >> pkgs/src.log || `echo $error; exit 1`
+
+echo "* Configuring"
+echo "#define TUXANCI_NG_VERSION \"${VERSION}\"" > $INSTALL_DIR/config.h || `echo $error; exit 1`
+
+rm -fv $INSTALL_DIR/ci >> pkgs/src.log || `echo $error; exit 1`
+
+echo "* Packaging"
+tar cjf $INSTALL_DIR.tar.bz2 $INSTALL_DIR || `echo $error; exit 1`
+
+echo "** Source package is ready!"
+
