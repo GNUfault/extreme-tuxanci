caf6b6d1f9528dd6d8cd02ab6d2173a0f5df2636
Tomas Chvatal (scarabeus) <tomas.chvatal@gmail.com>
2008-10-03 23:30:17 +0200

    Fixup last commit, missed one file :]

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ make clean
 find ./ -type d -name CMakeFiles -print |xargs -i rm {} -rf
 # remove cmake files
 find ./ \( -type f -name \*.cmake -print , -type f -name CMakeCache.txt -print , -type f -name Makefile -print \) |grep -v "./cmake" |xargs -i rm {} -rf
-find ./ -type f -name cmake_install.cmake |xargs -i rm {} -rf
+find ./ \( -type f -name cmake_install.cmake -print , -type f -name install_manifest.txt -print \) |xargs -i rm {} -rf
 # remove precompiled in files
 find ./ -name \*.in -print |while read r; do rm -rf ${r/\.in/}; done
 
