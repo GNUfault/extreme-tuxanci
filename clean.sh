0b4d9821c7333b8c4bdf7e0eeb750aef502a1137
xHire <xhire@tuxportal.cz>
2008-10-19 15:34:22 +0200

    Update of clean.sh + merged its changes from opengl branch by scarabeus

--- clean.sh
+++ clean.sh
@@ -7,9 +7,14 @@
 find ./ -type d -name CMakeFiles -print | xargs -i rm {} -rf
 
 # remove cmake files
-find ./ \( -type f -name \*.cmake -print , -type f -name CMakeCache.txt -print , -type f -name Makefile -print \) | grep -v "./cmake" | xargs -i rm {} -f
+find ./ \( -type f -name \*.so -print , -type f -name \*.cmake -print , -type f -name CMakeCache.txt -print , -type f -name Makefile -print \) | grep -v "./cmake" | xargs -i rm {} -f
 find ./ \( -type f -name install_manifest.txt -print , -type f -name cmake_install.cmake -print \) | xargs -i rm {} -f
 
 # remove precompiled in files
 find ./ -name \*.in -print | while read r; do rm -f ${r/\.in/}; done
 
+# remove compiled binaries
+find src -name tuxanci-\* -print | xargs -i rm {} -f
+
+# remove compiled gettext files
+find ./ -name \*.gmo -print | xargs -i rm {} -f
