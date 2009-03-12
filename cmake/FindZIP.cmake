2ab1ed0ed358916a6f463d43df465b9499bf8dea
Tomas Chvatal <scarabeus@gentoo.org>
2009-03-12 17:41:02 +0100

    Hooops missed some zz :]

--- cmake/FindZIP.cmake
+++ cmake/FindZIP.cmake
@@ -1,9 +1,9 @@
 # - Find zip
 # Find the native ZIP includes and library
 #
-#  ZIP_INCLUDE_DIR - where to find zzip.h, etc.
-#  ZIP_LIBRARIES   - List of libraries when using zzip.
-#  ZIP_FOUND       - True if zzip found.
+#  ZIP_INCLUDE_DIR - where to find zip.h, etc.
+#  ZIP_LIBRARIES   - List of libraries when using zip.
+#  ZIP_FOUND       - True if zip found.
 
 
 IF (ZIP_INCLUDE_DIR)
@@ -14,7 +14,7 @@ ENDIF (ZIP_INCLUDE_DIR)
 FIND_PATH(ZIP_INCLUDE_DIR zip.h)
 
 SET(ZIP_NAMES zip)
-FIND_LIBRARY(ZIP_LIBRARY NAMES ${ZZIP_NAMES} )
+FIND_LIBRARY(ZIP_LIBRARY NAMES ${ZIP_NAMES} )
 
 # handle the QUIETLY and REQUIRED arguments and set ZIP_FOUND to TRUE if 
 # all listed variables are TRUE
@@ -22,7 +22,7 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ZIP DEFAULT_MSG ZIP_LIBRARY ZIP_INCLUDE_DIR)
 
 IF(ZIP_FOUND)
-  SET( ZIP_LIBRARIES ${ZZIP_LIBRARY} )
+  SET( ZIP_LIBRARIES ${ZIP_LIBRARY} )
 ELSE(ZIP_FOUND)
    SET(ZIP_FOUND FALSE)
       IF (ZIP_REQUIRED)
