55763badae9118135126c4c623cd0c7f09183c09
Tomáš Chvátal <scarabeus@gentoo.org>
2009-06-21 19:38:15 +0200

    Use macro for sources addition. Fix typo in findzip.

---
+++ cmake/MacroAddSources.cmake
@@ -0,0 +1,9 @@
+# This macro is for generating the sources list from in-folder files.
+MACRO(MacroAddSources)
+	FILE ( GLOB src ${fn}/*.c )
+	FOREACH ( fle ${src} )
+		GET_FILENAME_COMPONENT ( fnp ${fle} NAME_WE )
+		GET_FILENAME_COMPONENT ( pth ${fle} PATH )
+		SET ( SRC_${fn} ${SRC_${fn}} ${pth}/${fnp} )
+	ENDFOREACH ( fle )
+ENDMACRO(MacroAddSources)
